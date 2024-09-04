4ea1c639178744a0d64dd1b3a8cc1a042d200f4c
Jordan-Iralde <iraldejordan10@gmail.com>
2024-09-04 12:10:17 -0300

    Comentarios Explicativos

--- JordanCPP/Ejercicio 1/Ejercicio1.cpp
+++ JordanCPP/Ejercicio 1/Ejercicio1.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 int main(){ //Puedo implementar la funcion Despues(Si hace falta)
     int a;
-    cout << "Ingrese la Variable: ";
-    cin  >> a;
+    cout << "Ingrese la Variable: ";    // como un print
+    cin  >> a;                          // como un input
 
     cout << "\n Ingrese la Base: ";
     int b;
     cin  >> b;
     
-    int numeros[100];
+    int numeros[100];                   // Una lista con capacidad para 100 numeros
     int index = 0;
     
     while (a > 0){
         int resto = a % b;
-        numeros[index] = resto;  // Almacena el resto en la posición correcta
-        index++; //Hasta que termine
+        numeros[index] = resto;         // Almacena el resto en la posición correcta
+        index++;                        // Hasta que termine incrementa su valor para el numero de iteraciones(100)
         cout << a << " % " << b << " = " << resto << endl;
-        a = a / b; //No se requiere de una floor division
+        a = a / b;                      // No se requiere de una floor division, las divisiones son exactas
         cout << "a es " << a << endl;
         
     }
