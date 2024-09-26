2c9b8d368b7df0f797782b9be1b123d0b520236e
Jordan Iralde <iraldejordan10@gmail.com>
2024-09-26 20:14:03 -0300

    Update filtranum.py

--- JordanCPP/Python/filtranum.py
+++ JordanCPP/Python/filtranum.py
@@ -2,14 +2,14 @@ numeros = [13, 101, 28883, 13, 345673, 55, 9, 110, 101]
 resultado = []
 
 def filtranum(numeros, resultado):
+    cantidad = 0
     for i in numeros:
         iposicion = str(i)
         if iposicion[0] == iposicion[-1]:
             resultado.append(int(iposicion))   
-        if i == 9999:
-            break
+            cantidad = cantidad + 1
 
 
-    return resultado
+    return cantidad
 if __name__ == '__main__':
     print(filtranum(numeros, resultado))
